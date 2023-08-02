0796ef66d69cc9b00f506e234dbf8a345f2a64cb
Alekseev Sergei <nec13@mail.ru>
2023-08-03 02:58:17 +0300

    Sprint 13

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const routerCard = require('./routes/cards');
 
 const errorStatus = require('./utils/errors');
 
-mongoose.connect('mongodb://localhost/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
