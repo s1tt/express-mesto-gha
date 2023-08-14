b0c0f313975ce1be083ee8eff9a49185cdc9d930
Alekseev Sergei <nec13@mail.ru>
2023-08-14 07:55:24 +0300

    PR 14

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const routerUser = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -9,9 +10,26 @@ const {
 } = require('../controllers/users');
 
 routerUser.get('/', getUsers);
-routerUser.get('/:userId', getUser);
+
+routerUser.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUser);
+
 routerUser.post('/', createUser);
-routerUser.patch('/me', updateProfile);
-routerUser.patch('/me/avatar', updateAvatar);
+
+routerUser.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
+
+routerUser.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().regex(/^(https?:\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w.-]*)*\/?$/),
+  }),
+}), updateAvatar);
 
 module.exports = routerUser;
