ee6e71016a36352b115efe6d6ea752d5e67b4e7a
Alekseev Sergei <nec13@mail.ru>
2023-08-14 08:01:15 +0300

    PR 14

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const routerCard = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getCards,
@@ -9,9 +10,30 @@ const {
 } = require('../controllers/cards');
 
 routerCard.get('/', getCards);
-routerCard.post('/', createCard);
-routerCard.delete('/:cardId', deleteCard);
-routerCard.put('/:cardId/likes', likeCard);
-routerCard.delete('/:cardId/likes', deleteLike);
+
+routerCard.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().regex(/^(https?:\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w.-]*)*\/?$/),
+  }),
+}), createCard);
+
+routerCard.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).required(),
+  }),
+}), deleteCard);
+
+routerCard.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).required(),
+  }),
+}), likeCard);
+
+routerCard.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).required(),
+  }),
+}), deleteLike);
 
 module.exports = routerCard;
